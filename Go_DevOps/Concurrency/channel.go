5523d63649bff95603223d22793d4511afe525c3
Ayokinsman <ayobami.ayinde@kinship.co>
2023-03-13 08:27:53 +0000

    select, mutexes

--- Go_DevOps/Concurrency/channel.go
+++ Go_DevOps/Concurrency/channel.go
@@ -24,3 +24,18 @@ func count(thing string, c chan string) {
 
 	close(c)
 }
+
+// func main() {
+// 	ch := make(chan string, 1)
+
+// 	go func() {
+// 		for _, word := range []string{"hello", "world"} {
+// 			ch <- word
+// 		}
+// 		close(ch)
+// 	}()
+
+// 	for word := range ch {
+// 		fmt.Println(word)
+// 	}
+// }
