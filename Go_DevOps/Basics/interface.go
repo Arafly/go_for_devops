d6fc33f39d275619bf89cf7d6644625572f62348
Ayokinsman <ayobami.ayinde@kinship.co>
2023-03-14 09:14:50 +0000

    interface starter

--- Go_DevOps/Basics/interface.go
+++ Go_DevOps/Basics/interface.go
@@ -1,3 +1,6 @@
+// Interfaces are a great way to enforce a blueprint for what your app can do, similar to object oriented programming.
+// Interfaces are a contract that defines what methods a type should have. They don't define data, they define behavior.
+
 package main
 
 // import (
@@ -6,5 +9,5 @@ package main
 // )	
 
 func main () {
-	
+
 }
