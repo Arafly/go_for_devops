8311e89b6c2a0c23f9e9e622a8fc335eb366b669
Ayokinsman <ayobami.ayinde@kinship.co>
2023-03-21 08:49:19 +0000

    the os package library

--- Go_DevOps/IO/write.go
+++ Go_DevOps/IO/write.go
@@ -11,6 +11,7 @@ import (
 // go doc os.File
 // go doc os.File.Write
 
+// The os.Create function creates a new file with the specified path. If the file already exists, os.Create will overwrite it
 func main() {
 	file, err := os.Create("goWrite.txt")
     if err != nil {
