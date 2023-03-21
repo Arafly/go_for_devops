db49e6a940f596adeb64c4b8183b9febd6070127
Ayokinsman <ayobami.ayinde@kinship.co>
2023-03-21 09:17:08 +0000

    OS-agnostic pathing

---
+++ Go_DevOps/IO/agnostic_path.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	// "runtime"
+	"os"
+	"path/filepath"
+)
+
+// Using the runtime package, you can detect the OS and platform you are running on:
+// func main() {
+// 	fmt.Println(runtime.GOOS) // linux, darwin, ...
+// 	fmt.Println(runtime.GOARCH) // amd64, arm64, ...
+// }
+
+// Say we want to access a configuration file - config.json, thats stored in the config/ directory, which is in the same directory as our binary.
+
+func main() {
+	wd, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error getting work directory:", err)
+		os.Exit(1)
+	}
+	content, err := os.ReadFile(filepath.Join(wd, "config", "config.json"))
+	fmt.Printf("%s", content)
+}
+
+// filepath.Join() allows us to join the components of our path into a single path. This fills in the OS-specific directory separators for you and uses the native pathing rules.
+
