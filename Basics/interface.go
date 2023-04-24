a3e70dcf4fc9e6f2a7fafa1735ad835dff491e21
Ayokinsman <ayobami.ayinde@kinship.co>
2023-04-24 07:19:27 +0100

    separation of concerns

---
+++ Basics/interface.go
@@ -0,0 +1,13 @@
+// Interfaces are a great way to enforce a blueprint for what your app can do, similar to object oriented programming.
+// Interfaces are a contract that defines what methods a type should have. They don't define data, they define behavior.
+
+package main
+
+// import (
+// 	"fmt"
+// 	"sync"
+// )	
+
+func main () {
+
+}
