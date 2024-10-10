79b42d949aff6afec0782c9d7730f2c846a4d6be
Ayokinsman <ayobami.ayinde@kinship.co>
2024-10-10 11:37:37 +0100

    [feat]: mocking terminated  :amazed:

---
+++ Basics/mocking/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+type DefaultSleeper struct {}
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+const write = "write"
+const sleep = "sleep"
+
+const (
+	finalWord = "Go!"
+	countdownStart = 3
+)
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+	}
+
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+	}
+
+	sleeper.Sleep()
+	fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
+}
--- go.mod
+++ go.mod
@@ -1,12 +1,10 @@
-module  go_for
+module go_for
 
 go 1.22.1
 
+require github.com/go-yaml/yaml v2.1.0+incompatible
+
 require (
-	github.com/kr/pretty v0.2.1 // indirect
-	github.com/kr/text v0.1.0 // indirect
-	github.com/yuin/goldmark v1.4.13 // indirect
-	golang.org/x/mod v0.17.0 // indirect
-	golang.org/x/tools v0.20.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
